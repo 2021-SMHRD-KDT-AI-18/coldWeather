0c7088da9c772035d06155404807a25638a90886
SMHRD <SMHRD@218.149.140.8>
2024-01-25 11:34:19 +0900

    커밋

--- GitTest0125/src/Ex01.java
+++ GitTest0125/src/Ex01.java
@@ -2,21 +2,10 @@
 public class Ex01 {
 
 	public static void main(String[] args) {
-
 		System.out.println("팀장 첫번째 커밋!");
 		System.out.println("팀원1 두번째 커밋!");
 		System.out.println("팀원 2222222222 커밋!");
 		System.out.println("팀원 3 커~밋");
-<<<<<<< HEAD
-		
-		
-		
-		
-		
-		
-=======
 		System.out.println("점심 뭐먹어요");
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-KDT-AI-18/nameless2.git
 	}
-
 }
