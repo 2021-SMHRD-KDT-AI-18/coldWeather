5aa177b641911ab7ada004acb35324018049926c
SMHRD <SMHRD@218.149.140.8>
2024-01-26 15:55:09 +0900

    민 DTO

--- GitTest0125/src/개발자괴롭히기/CharacterDTO.java
+++ GitTest0125/src/개발자괴롭히기/CharacterDTO.java
@@ -3,57 +3,58 @@ package 개발자괴롭히기;
 public class CharacterDTO {
 
 	// 필드
-		private int name;
-		private int hp;
-		private int intell;
-		private int fp;
-		private int mp;
-		private int ment;
-		private String salary;
-       
-		
-		// 생성자 메소드
+	private String name;
+	private int hp;
+	private int intell;
+	private int fp;
+	private int mp;
+	private int ment;
+	private int salary;
 
-		public CharacterDTO(int name, int hp, int intell, int fp, int mp, int ment, String salary) {
-			super();
-			this.name = name;
-			this.hp = hp;
-			this.intell = intell;
-			this.fp = fp;
-			this.mp = mp;
-			this.ment = ment;
-			this.salary = salary;
-		}
+//		name 메소드
+	public CharacterDTO(String name) {
+		super();
+		this.name = name;
+	}
 
-		// getter 메소드
+	public String getName() {
+		return name;
+	}
 
-		public int getName() {
-			return name;
-		}
+	// state 메소드
 
-		public int getHp() {
-			return hp;
-		}
+	public CharacterDTO(int hp, int intell, int fp, int mp, int ment, int salary) {
+		super();
+		this.hp = hp;
+		this.intell = intell;
+		this.fp = fp;
+		this.mp = mp;
+		this.ment = ment;
+		this.salary = salary;
+	}
 
-		public int getIntell() {
-			return intell;
-		}
+	public int getHp() {
+		return hp;
+	}
 
-		public int getFp() {
-			return fp;
-		}
-		
-		public int getMp() {
-			return mp;
-		}
-		
-		public int getMent() {
-			return ment;
-		}
-	
-		public String getSalary() {
-			return salary;
-		}
-	
-	
+	public int getIntell() {
+		return intell;
+	}
+
+	public int getFp() {
+		return fp;
+	}
+
+	public int getMp() {
+		return mp;
+	}
+
+	public int getMent() {
+		return ment;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
 }
