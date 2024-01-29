63c1b722e54a656261c8cda3206ede5981b72c67
[anhyunji494] <smhrd@119.200.31.111>
2024-01-29 10:51:45 +0900

    salay DESC 추가

--- GitTest0125/src/개발자괴롭히기/DeveloperMainSystem.java
+++ GitTest0125/src/개발자괴롭히기/DeveloperMainSystem.java
@@ -175,9 +175,11 @@ public class DeveloperMainSystem {
 
 			else if (menu == 2) {
 				System.out.println("====랭킹 조회====");
-				ArrayList<CharacterDTO> list = controller.CharacterList();// 나중에 characterDTO로 변경
+				ArrayList<String> list = controller.CharacterList();// 나중에 characterDTO로 변경
+				System.out.println(list.size());
+				System.out.println(list.get(1));
 				for (int i = 0; i < list.size(); i++) {
-					System.out.print((i+1) +"위>>" + list.get(i).getName() + "\t");
+					System.out.print((i+1) +"위>>" + list.get(i) + "\t");
 
 				}
 				System.out.println();
